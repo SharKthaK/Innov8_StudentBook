42832c403beb27f6753a51444bac6e739fff7d29
BASUDEV DAS <dbasudev329@gmail.com>
2024-09-13 13:11:31 +0530

    StudentBook

--- Frontend/src/components/pages/departments/CSEAI.jsx
+++ Frontend/src/components/pages/departments/CSEAI.jsx
@@ -1,53 +1,78 @@
-import React, {useState} from 'react'
-import { useNavigate } from 'react-router-dom';
-import data from '../../../Batch.json'
-import '../Home.css'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import data from "../../../Batch.json";
+import "../Home.css";
 
 function CSEAI() {
   const [searchTerm, setSearchTerm] = useState("");
   const navigate = useNavigate();
   return (
-    <div className='templateContainer' style={{ backgroundImage: 'url(https://tse4.mm.bing.net/th?id=OIP.z9vQwb9fX6aB6KX3E-rd9gHaE0&pid=Api&P=0&h=180)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
-      <div className='searchInput_Container'>
-        <input type='text' placeholder='Search batch...' onChange={(event)=>{
-          setSearchTerm(event.target.value)
-        }}></input>
+    <div
+      className="templateContainer"
+      style={{
+        backgroundImage:
+          "url(https://tse4.mm.bing.net/th?id=OIP.z9vQwb9fX6aB6KX3E-rd9gHaE0&pid=Api&P=0&h=180)",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="searchInput_Container">
+        <input
+          type="text"
+          placeholder="Search batch..."
+          onChange={(event) => {
+            setSearchTerm(event.target.value);
+          }}
+        ></input>
       </div>
-      
-        <div className='template_Container'>
-          {
-            data.filter((val)=>{
-              if(searchTerm === ""){
-                return val
-              } else if (val.name.toLowerCase().includes(searchTerm.toLowerCase())){
-                return val
-              }
-            })
-            .map((val)=>{
-              return (
-                <>
-                
-                <div className="template" key={val.id} style={{ boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.2)' }} onClick={()=>{
-                  navigate(`/${val.id}/${val.name}`)
-          
-                }}>
-                  <img src={val.img} style={{ border: '3px solid grey', borderRadius: '20px', transition: 'transform 0.3s', width:'200px' }} onMouseOver={(event) => {
-                    event.target.style.transform = 'scale(1.1)';
-                  }} onMouseOut={(event) => {
-                    event.target.style.transform = 'scale(1)';
-                  }}/>
+
+      <div className="template_Container">
+        {data
+          .filter((val) => {
+            if (searchTerm === "") {
+              return val;
+            } else if (
+              val.name.toLowerCase().includes(searchTerm.toLowerCase())
+            ) {
+              return val;
+            }
+          })
+          .map((val) => {
+            return (
+              <>
+                <div
+                  className="template"
+                  key={val.id}
+                  style={{ boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.2)" }}
+                  onClick={() => {
+                    navigate(`/${val.id}/${val.name}`);
+                  }}
+                >
+                  <img
+                    src={val.img}
+                    style={{
+                      border: "3px solid grey",
+                      borderRadius: "20px",
+                      transition: "transform 0.3s",
+                      width: "200px",
+                    }}
+                    onMouseOver={(event) => {
+                      event.target.style.transform = "scale(1.1)";
+                    }}
+                    onMouseOut={(event) => {
+                      event.target.style.transform = "scale(1)";
+                    }}
+                  />
                   <br />
                   <h6>B.TECH CSE-AI</h6>
                   <h3>{val.name}</h3>
                 </div>
-                </>
-              );
-            })
-          }
-        
+              </>
+            );
+          })}
       </div>
     </div>
-  )
+  );
 }
 
-export default CSEAI
+export default CSEAI;
