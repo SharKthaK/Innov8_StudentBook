98474989884421e02399713d895c32e958feb5e8
BASUDEV DAS <dbasudev329@gmail.com>
2024-09-13 13:23:57 +0530

    css added of cseAI

--- Frontend/src/components/pages/departments/CSEAI.jsx
+++ Frontend/src/components/pages/departments/CSEAI.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import data from "../../../Batch.json";
-import "../Home.css";
+import "./CSEAI.css";
 
 function CSEAI() {
   const [searchTerm, setSearchTerm] = useState("");
